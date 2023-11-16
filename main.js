6624785c200e3e66ab0e4f8b029a8210c2465338
byron dray <byrondray8@gmail.com>
2023-11-15 19:38:48 -0800

    removed unused path variable

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ const path = require("path");
 const { unzip, readDir, chooseFilter } = require("./IOhandler");
 const zipFilePath = path.join(__dirname, "myfile.zip");
 const pathUnzipped = path.join(__dirname, "unzipped");
-const pathProcessed = path.join(__dirname, "grayscaled");
 
 unzip(zipFilePath, pathUnzipped)
   .then(() => readDir(pathUnzipped))
